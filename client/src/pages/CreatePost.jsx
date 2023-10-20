9b4d5a02098c648f0d56116631bc347b92a5f811
sforshing <sforshing@gmail.com>
2023-10-19 23:10:55 -0400

    update server and API

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dall-e-aaid.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://dalle-wohm.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://dall-e-aaid.onrender.com/api/v1/post', {
+        const response = await fetch('https://dalle-wohm.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
